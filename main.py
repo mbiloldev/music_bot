7551fb323138d92ad40e98685023c5180449b168
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-15 08:51:35 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from handlers import start, search
+from utils.cleanup import clean_downloads
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    clean_downloads()
+
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(start.router)
+    dp.include_router(search.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
