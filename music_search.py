c368c82a3cfbb6ecda75ef149f34f7052e99b936
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-15 09:02:00 +0500

    Update music_search.py

--- music_search.py
+++ music_search.py
@@ -1,9 +1,1 @@
-import yt_dlp
-
-
-def search_music(query: str, limit: int = 5) -> list[dict]:
-    """Berilgan so'rov bo'yicha YouTube'dan qo'shiqlarni qidiradi."""
-    ydl_opts = {
-
-    w
-    return results
+ff
